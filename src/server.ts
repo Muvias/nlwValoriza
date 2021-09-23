b3ed3d49c48eb654393718effb1f9a57691d53ac
Vinicius Perazza <vi_guepardo@hotmail.com>
2021-09-23 20:16:01 -0300

    nlwValoriza

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import "reflect-metadata";
-import express from "express";
+import express, { Request, Response, NextFunction } from "express";
+import "express-async-errors";
 
 import { router } from "./routes"
 
@@ -11,6 +12,24 @@ app.use(express.json())
 
 app.use(router);
 
+// tratamento de erros vai ser feito por aqui:
+// Erros como usuário cadastrado já existente e etc
+app.use((err: Error, request: Request, response: Response, next: NextFunction) => {
+    if(err instanceof Error){
+        return response.status(400).json({
+            error: err.message
+        })
+    }
+
+    return response.status(500).json({
+        status: "error",
+        message: "Internal Server Error"
+    })
+})
+
+app.listen(3000, () => console.log("Server is running"));
+
+
 /**
  * GET => Buscar uma informação
  * POST => Inserir (criar) uma informação
@@ -18,5 +37,3 @@ app.use(router);
  * DELETE => Remover um dado
  * PATCH => Alterar uma informação específica
  */
-
-app.listen(3000, () => console.log("Server is running"));
